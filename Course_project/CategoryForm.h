fbeadbd5ff442674cef5df77b9d74b81063db2ad
Кузовников Роман Петрович <9132253625x@gmail.com>
2025-01-08 15:08:59 +0700

    Оформление формы для создания новой категории

--- Course_project/CategoryForm.h
+++ Course_project/CategoryForm.h
@@ -37,8 +37,10 @@ namespace Courseproject {
 	protected:
 	private: System::Windows::Forms::Button^ buttonCreateCategory;
 	private: System::Windows::Forms::TextBox^ textBoxCategoryDescription;
-	private: System::Windows::Forms::Label^ label1;
-	private: System::Windows::Forms::MenuStrip^ menuStrip1;
+	private: System::Windows::Forms::Label^ label_new_category;
+	private: System::Windows::Forms::MenuStrip^ menuStrip_new_category;
+
+
 	private: System::Windows::Forms::ToolStripMenuItem^ информацияToolStripMenuItem;
 	private: System::Windows::Forms::ToolStripMenuItem^ вернутьсяНазадToolStripMenuItem;
 
@@ -47,6 +49,8 @@ namespace Courseproject {
 		/// Обязательная переменная конструктора.
 		/// </summary>
 		System::ComponentModel::Container ^components;
+	private: System::Windows::Forms::Label^ label_name_category;
+	private: System::Windows::Forms::Label^ label_opis_category;
 	public:
 		System::String^ categoryFilePath;
 		// Метод для сохранения категорий в файл
@@ -64,58 +68,60 @@ namespace Courseproject {
 			this->textBoxCategoryName = (gcnew System::Windows::Forms::TextBox());
 			this->buttonCreateCategory = (gcnew System::Windows::Forms::Button());
 			this->textBoxCategoryDescription = (gcnew System::Windows::Forms::TextBox());
-			this->label1 = (gcnew System::Windows::Forms::Label());
-			this->menuStrip1 = (gcnew System::Windows::Forms::MenuStrip());
+			this->label_new_category = (gcnew System::Windows::Forms::Label());
+			this->menuStrip_new_category = (gcnew System::Windows::Forms::MenuStrip());
 			this->информацияToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
 			this->вернутьсяНазадToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
-			this->menuStrip1->SuspendLayout();
+			this->label_name_category = (gcnew System::Windows::Forms::Label());
+			this->label_opis_category = (gcnew System::Windows::Forms::Label());
+			this->menuStrip_new_category->SuspendLayout();
 			this->SuspendLayout();
 			// 
 			// textBoxCategoryName
 			// 
-			this->textBoxCategoryName->Location = System::Drawing::Point(343, 200);
+			this->textBoxCategoryName->Location = System::Drawing::Point(246, 213);
 			this->textBoxCategoryName->Name = L"textBoxCategoryName";
 			this->textBoxCategoryName->Size = System::Drawing::Size(100, 22);
 			this->textBoxCategoryName->TabIndex = 0;
 			// 
 			// buttonCreateCategory
 			// 
-			this->buttonCreateCategory->Location = System::Drawing::Point(356, 356);
+			this->buttonCreateCategory->Location = System::Drawing::Point(856, 473);
 			this->buttonCreateCategory->Name = L"buttonCreateCategory";
-			this->buttonCreateCategory->Size = System::Drawing::Size(75, 23);
+			this->buttonCreateCategory->Size = System::Drawing::Size(236, 64);
 			this->buttonCreateCategory->TabIndex = 1;
-			this->buttonCreateCategory->Text = L"button1";
+			this->buttonCreateCategory->Text = L"Добавить категорию";
 			this->buttonCreateCategory->UseVisualStyleBackColor = true;
 			this->buttonCreateCategory->Click += gcnew System::EventHandler(this, &CategoryForm::buttonCreateCategory_Click);
 			// 
 			// textBoxCategoryDescription
 			// 
-			this->textBoxCategoryDescription->Location = System::Drawing::Point(539, 200);
+			this->textBoxCategoryDescription->Location = System::Drawing::Point(246, 393);
 			this->textBoxCategoryDescription->Name = L"textBoxCategoryDescription";
 			this->textBoxCategoryDescription->Size = System::Drawing::Size(100, 22);
 			this->textBoxCategoryDescription->TabIndex = 2;
 			// 
-			// label1
+			// label_new_category
 			// 
-			this->label1->AutoSize = true;
-			this->label1->Location = System::Drawing::Point(505, 77);
-			this->label1->Name = L"label1";
-			this->label1->Size = System::Drawing::Size(46, 17);
-			this->label1->TabIndex = 3;
-			this->label1->Text = L"label1";
+			this->label_new_category->AutoSize = true;
+			this->label_new_category->Location = System::Drawing::Point(491, 60);
+			this->label_new_category->Name = L"label_new_category";
+			this->label_new_category->Size = System::Drawing::Size(186, 17);
+			this->label_new_category->TabIndex = 3;
+			this->label_new_category->Text = L"Создание новой категории";
 			// 
-			// menuStrip1
+			// menuStrip_new_category
 			// 
-			this->menuStrip1->ImageScalingSize = System::Drawing::Size(20, 20);
-			this->menuStrip1->Items->AddRange(gcnew cli::array< System::Windows::Forms::ToolStripItem^  >(2) {
+			this->menuStrip_new_category->ImageScalingSize = System::Drawing::Size(20, 20);
+			this->menuStrip_new_category->Items->AddRange(gcnew cli::array< System::Windows::Forms::ToolStripItem^  >(2) {
 				this->информацияToolStripMenuItem,
 					this->вернутьсяНазадToolStripMenuItem
 			});
-			this->menuStrip1->Location = System::Drawing::Point(0, 0);
-			this->menuStrip1->Name = L"menuStrip1";
-			this->menuStrip1->Size = System::Drawing::Size(1201, 28);
-			this->menuStrip1->TabIndex = 4;
-			this->menuStrip1->Text = L"menuStrip1";
+			this->menuStrip_new_category->Location = System::Drawing::Point(0, 0);
+			this->menuStrip_new_category->Name = L"menuStrip_new_category";
+			this->menuStrip_new_category->Size = System::Drawing::Size(1201, 28);
+			this->menuStrip_new_category->TabIndex = 4;
+			this->menuStrip_new_category->Text = L"menuStrip1";
 			// 
 			// информацияToolStripMenuItem
 			// 
@@ -130,21 +136,41 @@ namespace Courseproject {
 			this->вернутьсяНазадToolStripMenuItem->Text = L"Вернуться назад";
 			this->вернутьсяНазадToolStripMenuItem->Click += gcnew System::EventHandler(this, &CategoryForm::вернутьсяНазадToolStripMenuItem_Click);
 			// 
+			// label_name_category
+			// 
+			this->label_name_category->AutoSize = true;
+			this->label_name_category->Location = System::Drawing::Point(246, 147);
+			this->label_name_category->Name = L"label_name_category";
+			this->label_name_category->Size = System::Drawing::Size(204, 17);
+			this->label_name_category->TabIndex = 5;
+			this->label_name_category->Text = L"Введите название категории:";
+			// 
+			// label_opis_category
+			// 
+			this->label_opis_category->AutoSize = true;
+			this->label_opis_category->Location = System::Drawing::Point(246, 341);
+			this->label_opis_category->Name = L"label_opis_category";
+			this->label_opis_category->Size = System::Drawing::Size(201, 17);
+			this->label_opis_category->TabIndex = 6;
+			this->label_opis_category->Text = L"Введите описание категории";
+			// 
 			// CategoryForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->ClientSize = System::Drawing::Size(1201, 658);
-			this->Controls->Add(this->label1);
+			this->Controls->Add(this->label_opis_category);
+			this->Controls->Add(this->label_name_category);
+			this->Controls->Add(this->label_new_category);
 			this->Controls->Add(this->textBoxCategoryDescription);
 			this->Controls->Add(this->buttonCreateCategory);
 			this->Controls->Add(this->textBoxCategoryName);
-			this->Controls->Add(this->menuStrip1);
-			this->MainMenuStrip = this->menuStrip1;
+			this->Controls->Add(this->menuStrip_new_category);
+			this->MainMenuStrip = this->menuStrip_new_category;
 			this->Name = L"CategoryForm";
-			this->Text = L"CategoryForm";
-			this->menuStrip1->ResumeLayout(false);
-			this->menuStrip1->PerformLayout();
+			this->Text = L"Новая категория";
+			this->menuStrip_new_category->ResumeLayout(false);
+			this->menuStrip_new_category->PerformLayout();
 			this->ResumeLayout(false);
 			this->PerformLayout();
 
