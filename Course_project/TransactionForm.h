f5959cbe1987eb682755232c8e1f786a02cb45d5
Кузовников Роман Петрович <9132253625x@gmail.com>
2025-01-10 00:02:14 +0700

    Добавление понятного интерфейса для пользователя.

--- Course_project/TransactionForm.h
+++ Course_project/TransactionForm.h
@@ -61,6 +61,12 @@ namespace Courseproject {
 	private: System::Windows::Forms::ToolStripMenuItem^ информацияToolStripMenuItem;
 	private: System::Windows::Forms::ToolStripMenuItem^ вернутьсяНазадToolStripMenuItem;
 	private: System::Windows::Forms::DateTimePicker^ dateTimePicker_Date;
+	private: System::Windows::Forms::Label^ label2;
+	private: System::Windows::Forms::Label^ label3;
+	private: System::Windows::Forms::Label^ label4;
+	private: System::Windows::Forms::Label^ label5;
+	private: System::Windows::Forms::Label^ label6;
+	private: System::Windows::Forms::Label^ label7;
 	private: System::Windows::Forms::ComboBox^ comboBox_Account;
 		
 	public:
@@ -98,6 +104,12 @@ namespace Courseproject {
 			this->вернутьсяНазадToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
 			this->dateTimePicker_Date = (gcnew System::Windows::Forms::DateTimePicker());
 			this->comboBox_Account = (gcnew System::Windows::Forms::ComboBox());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->label3 = (gcnew System::Windows::Forms::Label());
+			this->label4 = (gcnew System::Windows::Forms::Label());
+			this->label5 = (gcnew System::Windows::Forms::Label());
+			this->label6 = (gcnew System::Windows::Forms::Label());
+			this->label7 = (gcnew System::Windows::Forms::Label());
 			this->menuStrip1->SuspendLayout();
 			this->SuspendLayout();
 			// 
@@ -113,23 +125,25 @@ namespace Courseproject {
 			// 
 			// label1
 			// 
-			this->label1->AutoSize = true;
-			this->label1->Location = System::Drawing::Point(652, 124);
+			this->label1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 16.2F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label1->Location = System::Drawing::Point(461, 90);
 			this->label1->Name = L"label1";
-			this->label1->Size = System::Drawing::Size(46, 17);
+			this->label1->Size = System::Drawing::Size(460, 53);
 			this->label1->TabIndex = 1;
-			this->label1->Text = L"label1";
+			this->label1->Text = L"Создание новой транзакции";
+			
 			// 
 			// textBox_TransactionText
 			// 
-			this->textBox_TransactionText->Location = System::Drawing::Point(287, 339);
+			this->textBox_TransactionText->Location = System::Drawing::Point(252, 325);
 			this->textBox_TransactionText->Name = L"textBox_TransactionText";
 			this->textBox_TransactionText->Size = System::Drawing::Size(100, 22);
 			this->textBox_TransactionText->TabIndex = 2;
 			// 
 			// textBox_TransactionAmount
 			// 
-			this->textBox_TransactionAmount->Location = System::Drawing::Point(287, 455);
+			this->textBox_TransactionAmount->Location = System::Drawing::Point(252, 454);
 			this->textBox_TransactionAmount->Name = L"textBox_TransactionAmount";
 			this->textBox_TransactionAmount->Size = System::Drawing::Size(100, 22);
 			this->textBox_TransactionAmount->TabIndex = 3;
@@ -138,7 +152,7 @@ namespace Courseproject {
 			// 
 			this->comboBox_TransactionType->FormattingEnabled = true;
 			this->comboBox_TransactionType->Items->AddRange(gcnew cli::array< System::Object^  >(2) { L"Доход", L"Расход" });
-			this->comboBox_TransactionType->Location = System::Drawing::Point(633, 455);
+			this->comboBox_TransactionType->Location = System::Drawing::Point(598, 452);
 			this->comboBox_TransactionType->Name = L"comboBox_TransactionType";
 			this->comboBox_TransactionType->Size = System::Drawing::Size(121, 24);
 			this->comboBox_TransactionType->TabIndex = 5;
@@ -146,7 +160,7 @@ namespace Courseproject {
 			// comboBox_Category
 			// 
 			this->comboBox_Category->FormattingEnabled = true;
-			this->comboBox_Category->Location = System::Drawing::Point(964, 339);
+			this->comboBox_Category->Location = System::Drawing::Point(929, 323);
 			this->comboBox_Category->Name = L"comboBox_Category";
 			this->comboBox_Category->Size = System::Drawing::Size(121, 24);
 			this->comboBox_Category->TabIndex = 6;
@@ -180,7 +194,7 @@ namespace Courseproject {
 			// 
 			// dateTimePicker_Date
 			// 
-			this->dateTimePicker_Date->Location = System::Drawing::Point(612, 337);
+			this->dateTimePicker_Date->Location = System::Drawing::Point(578, 323);
 			this->dateTimePicker_Date->Name = L"dateTimePicker_Date";
 			this->dateTimePicker_Date->Size = System::Drawing::Size(200, 22);
 			this->dateTimePicker_Date->TabIndex = 9;
@@ -188,16 +202,76 @@ namespace Courseproject {
 			// comboBox_Account
 			// 
 			this->comboBox_Account->FormattingEnabled = true;
-			this->comboBox_Account->Location = System::Drawing::Point(964, 455);
+			this->comboBox_Account->Location = System::Drawing::Point(929, 454);
 			this->comboBox_Account->Name = L"comboBox_Account";
 			this->comboBox_Account->Size = System::Drawing::Size(121, 24);
 			this->comboBox_Account->TabIndex = 10;
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Location = System::Drawing::Point(252, 267);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(157, 17);
+			this->label2->TabIndex = 11;
+			this->label2->Text = L"Название транзакции:";
+			// 
+			// label3
+			// 
+			this->label3->AutoSize = true;
+			this->label3->Location = System::Drawing::Point(255, 411);
+			this->label3->Name = L"label3";
+			this->label3->Size = System::Drawing::Size(54, 17);
+			this->label3->TabIndex = 12;
+			this->label3->Text = L"Сумма:";
+			// 
+			// label4
+			// 
+			this->label4->AutoSize = true;
+			this->label4->Location = System::Drawing::Point(608, 267);
+			this->label4->Name = L"label4";
+			this->label4->Size = System::Drawing::Size(123, 17);
+			this->label4->TabIndex = 13;
+			this->label4->Text = L"Дата транзакции";
+			// 
+			// label5
+			// 
+			this->label5->AutoSize = true;
+			this->label5->Location = System::Drawing::Point(595, 409);
+			this->label5->Name = L"label5";
+			this->label5->Size = System::Drawing::Size(124, 17);
+			this->label5->TabIndex = 14;
+			this->label5->Text = L"Тип доход/расход";
+			// 
+			// label6
+			// 
+			this->label6->AutoSize = true;
+			this->label6->Location = System::Drawing::Point(926, 267);
+			this->label6->Name = L"label6";
+			this->label6->Size = System::Drawing::Size(81, 17);
+			this->label6->TabIndex = 15;
+			this->label6->Text = L"Категория:";
+			// 
+			// label7
+			// 
+			this->label7->AutoSize = true;
+			this->label7->Location = System::Drawing::Point(926, 409);
+			this->label7->Name = L"label7";
+			this->label7->Size = System::Drawing::Size(44, 17);
+			this->label7->TabIndex = 16;
+			this->label7->Text = L"Счёт:";
 			// 
 			// TransactionForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->ClientSize = System::Drawing::Size(1378, 689);
+			this->Controls->Add(this->label7);
+			this->Controls->Add(this->label6);
+			this->Controls->Add(this->label5);
+			this->Controls->Add(this->label4);
+			this->Controls->Add(this->label3);
+			this->Controls->Add(this->label2);
 			this->Controls->Add(this->comboBox_Account);
 			this->Controls->Add(this->dateTimePicker_Date);
 			this->Controls->Add(this->comboBox_Category);
@@ -220,5 +294,6 @@ namespace Courseproject {
 	private: System::Void button_TransactionCreat_Click(System::Object^ sender, System::EventArgs^ e);
 private: System::Void вернутьсяНазадToolStripMenuItem_Click(System::Object^ sender, System::EventArgs^ e);
 private: System::Void информацияToolStripMenuItem_Click(System::Object^ sender, System::EventArgs^ e);
+
 };
 }
